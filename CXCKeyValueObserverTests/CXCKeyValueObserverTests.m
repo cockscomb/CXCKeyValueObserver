883134b1b1df3a109778b4fc9aa6169e8af3d844
cockscomb <cockscomb@hatena.ne.jp>
2014-02-08 14:33:46 +0900

    Change tests to use Specta

--- CXCKeyValueObserverTests/CXCKeyValueObserverTests.m
+++ CXCKeyValueObserverTests/CXCKeyValueObserverTests.m
@@ -6,29 +6,14 @@
 //
 //
 
-#import <XCTest/XCTest.h>
+#define EXP_SHORTHAND
 
-@interface CXCKeyValueObserverTests : XCTestCase
+#import "Specta.h"
+#import "Expecta.h"
 
-@end
+SpecBegin(KeyValueObserver)
 
-@implementation CXCKeyValueObserverTests
+describe(@"KeyValueObserver", ^{
+});
 
-- (void)setUp
-{
-    [super setUp];
-    // Put setup code here. This method is called before the invocation of each test method in the class.
-}
-
-- (void)tearDown
-{
-    // Put teardown code here. This method is called after the invocation of each test method in the class.
-    [super tearDown];
-}
-
-- (void)testExample
-{
-    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
-}
-
-@end
+SpecEnd
