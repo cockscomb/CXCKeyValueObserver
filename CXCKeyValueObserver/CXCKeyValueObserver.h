0533a8fc60c9da575cafe68f2db9c7346cc74f26
cockscomb <cockscomb@hatena.ne.jp>
2014-02-08 16:36:50 +0900

    Add silly documents

--- CXCKeyValueObserver/CXCKeyValueObserver.h
+++ CXCKeyValueObserver/CXCKeyValueObserver.h
@@ -8,10 +8,32 @@
 
 #import <Foundation/Foundation.h>
 
+/**
+ *  Block invoked when KVO fired.
+ *
+ *  @param observee The observed object.
+ *  @param keyPath  The observing keyPath.
+ *  @param change   A dictionary describing the changes.
+ *
+ *  @return `void`.
+ */
 typedef void (^CXCKeyValueObservingBlock) (id observee, NSString *keyPath, NSDictionary *change);
 
+/**
+ *  Observer using KVO.
+ */
 @interface CXCKeyValueObserver : NSObject
 
+/**
+ *  Basic initialization.
+ *
+ *  @param observee An object observed.
+ *  @param keyPath  The keyPath for observing.
+ *  @param options  `NSKeyValueObservingOptions`.
+ *  @param block    The block invoked when KVO fired.
+ *
+ *  @return Initialized instance of CXCKeyValueObserver.
+ */
 - (instancetype)initWithObservee:(id)observee forKeyPath:(NSString *)keyPath options:(NSKeyValueObservingOptions)options block:(CXCKeyValueObservingBlock)block;
 
 @end
